bda9a6e905c1281c00adaaa3e8865ba9bea783a3
Christopher Viet <78432542+cviet@users.noreply.github.com>
2021-11-04 14:42:37 +0100

    Main terminé, tous tests effectués

--- src/up/mi/acs/Main.java
+++ src/up/mi/acs/Main.java
@@ -6,37 +6,29 @@ import java.util.Scanner;
 public class Main {
 	
 	
-	
-	
 	public static void main(String[] args) {
 
-
-		
-		
 		Scanner sc = new Scanner(System.in);
 		
-		int nombre_pirate=-1;
+		int nombre_pirate=-1; 
 		
 		
-		while(nombre_pirate<0 | nombre_pirate > 26) {
+		while(nombre_pirate<1 | nombre_pirate > 26) {
 			System.out.println("Bonjour, quel est le nombre de pirates ?");
 			nombre_pirate = sc.nextInt();
 			
-			if(nombre_pirate < 0 | nombre_pirate > 26) {
-				System.out.println("Le nombre de pirates doit être supérieur à 0 et inférieur à 26");
+			if(nombre_pirate < 1 | nombre_pirate > 26) {
+				System.out.println("Le nombre de pirates doit être compris en 1 et 26.");
 			}
 	
 		}
 		
 		
-		
-		
-		
 		Equipage equipage = new Equipage(nombre_pirate);
 		
 		
-		int reponse=-1;
-		boolean termine = false;
+		int reponse=-1; // valeur du default
+		boolean termine = false; 
 		
 		while(!termine) {
 			reponse = Menu(sc);
@@ -83,21 +75,35 @@ public class Main {
 			if(reponse == 2) {
 				
 				boolean validation = false;
+				int i = 0;  
+				// compteur qui sert à ne pas déclencher le message d'erreur du else ci-dessous avant l'écriture dans le menu. 
+				// En effet, le sc.nextLine() fait faire un passage dans la boucle do while avant de récupérer ce qui est entré par l'utilisateur.
+				// Ce problème n'existe pas ailleurs car on utilise sc.next(), or on ne peut pas faire cela ici  car cela produit une erreur lorsque l'on fait .split()
 				
 				System.out.println("Pour ajouter des préferences à un pirate, merci de suivre la syntaxe suivante :");
 				System.out.println("idPIRATE pref1 pref2 ... prefn");
+				// on indique la syntaxe correcte pour que le split(" ") fonctionne correctement
+				// Par exemple , il faut taper 'A 1 2 3' dans le cas où nombre_pirate =3.
+				
 				do{
+					i++;
 					
-
 					String preference = sc.nextLine();
 					String[] preferences = preference.split(" ");	
+					int tests = 0; // compteur qui sert à vérifier que chacune des préférences entrées au clavier est valable.
 					
-					/*if(preferences.length == (nombre_pirate + 1)) {
-						validation = true;
-					}*/
-					//validation =false;
 					if((equipage.findPirateByID(preferences[0]) != null) && (preferences.length == (nombre_pirate + 1))) {
-						validation = true;
+						for (int j = 1; j< preferences.length; j++) {
+							if ( (preferences[j].matches("-?\\d+")) && (Integer.parseInt(preferences[j]) <= nombre_pirate )) {
+								// on vérifie que les préférences entrées sont bien des entiers avec  .matches(), le "-?\\d+" est une syntaxe trouvée après recherche web
+								// et on vérifie que l'entier en question est bien inférieur au nombre de pirates, càd correspond à un objet qu'il est possible d'attribuer.
+								tests++;
+							}
+						}
+						
+						if (tests == nombre_pirate) {
+							validation = true;
+						}
 					}
 					
 					if(validation) {
@@ -106,16 +112,16 @@ public class Main {
 						System.out.println("Les préférences du pirate " + A.getID() + " ont été mises à jour.");
 						
 					}
-					/*else {
+					if ( (!validation ) && i>1)  {
 						System.out.println("Syntaxe incorrecte, les préférences du pirate n'ont pas été mises à jour.");
-					}*/
+					}
 					
 				} while(validation != true);
 				
 				
 			}
 			
-		//}
+		
 		
 			if(reponse ==3) {
 				boolean verify = true;
@@ -127,7 +133,7 @@ public class Main {
 						if(pref[j] == null) {
 							verify = false;
 							System.out.println("Il manque une préférence pour le pirate " + tmp.getID());
-						}
+						} // Cette boucle vérifie que chaque pirate possède bien n préférences. Elle affiche un message pour chaque préférence manquante.
 					}
 					
 				}
@@ -145,7 +151,7 @@ public class Main {
 			}
 			if ( reponse == -1) {
 				System.out.println("Votre réponse n'est pas valide");
-			}
+			} // le default du switch du menu renvoie -1, donc toutes valeurs qui ne sont pas entre 1 et 3 sont renvoyés dans ce if.
 		} 
 				
 				boolean termine2 = false;
@@ -154,7 +160,7 @@ public class Main {
 						
 					
 						if(reponse == 1) {
-							//String B;
+							
 							Pirate Ap=null,Bp=null;
 							boolean pirate_exist = false;	
 							
@@ -201,7 +207,9 @@ public class Main {
 						
 						for(int i=0;i<nombre_pirate;i++) {
 							System.out.println(equipage.getEquipage().get(i).getID() + ":" + "o"+equipage.objet_recu.get(equipage.getEquipage().get(i)));
-							}	
+							}	// affiche les objets que possède chaque pirate après chaque action ( 1 ou 2 ). 
+								// Nous avons déterminés à la lecture du sujet qu'il n'était pas nécessaire de le faire après l'affectation naive,
+								// car il serait logique de s'intéresser au coût de cette dernière avant d'effectuer les échanges d'objets.
 						
 						if(reponse == 3) {
 							termine2 = true;
@@ -213,19 +221,11 @@ public class Main {
 						}
 				
 					} sc.close();
-			/*}
-			else {
-				System.out.println("Erreur : Vous n'avez pas encore ajouté toutes les préférences pour chacun des pirates.");
-				System.out.println("Veuillez ajouter les préférences manquantes avant de continuer. ");
-				
-			}*/
-	
+			
 	}
 	
 	
 	
-	
-
 	
 	
 	
